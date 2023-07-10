eb35a98aa19fa8afaab5dcdc120f84b8ea80b170
Paul Tirk <paultirk@paultirk.com>
2023-07-10 20:28:32 +0200

    add localforage to store

--- src/stores/Store.ts
+++ src/stores/Store.ts
@@ -1,14 +1,23 @@
 import { createTable } from 'blinkdb';
 
+import { createInstance } from 'localforage';
+
 import { useBlinkDB } from './blinkdb';
 
 export class Store<T extends Entity> {
 
   private table;
 
+  private localStorageTable;
+
   constructor(private readonly tableName: string) {
     const db = useBlinkDB();
     this.table = createTable<T>(db, tableName)();
+
+    this.localStorageTable = createInstance({
+      name: 'yatoo',
+      storeName: this.tableName
+    });
   }
 }
 
