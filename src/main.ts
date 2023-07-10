7ae4271ed33164d784871293b04023d08251be49
Paul Tirk <paultirk@paultirk.com>
2023-07-10 22:03:47 +0200

    use lucide icons instead of fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
 import { createApp, h, Suspense } from 'vue';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome';
-
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-
 import App from './App.vue';
 
 import router from './router';
@@ -27,12 +20,6 @@ const app = createApp({
 });
 
 app.use(router);
-
-library.add(fas, far, fab);
-
-app.component('FontAwesomeIcon', FontAwesomeIcon);
-app.component('FontAwesomeLayers', FontAwesomeLayers);
-app.component('FontAwesomeLayersText', FontAwesomeLayersText);
 
 declare const COMMIT_HASH: string;
 app.provide('COMMIT_HASH', COMMIT_HASH);
