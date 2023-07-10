1587d79fb2569996eaea9e2bda7ec74f96dd1c10
Paul Tirk <paultirk@paultirk.com>
2023-07-10 11:01:52 +0200

    add fontawesome icons component

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp, h, Suspense } from 'vue';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
 import App from './App.vue';
 
 import router from './router';
@@ -20,5 +27,11 @@ const app = createApp({
 });
 
 app.use(router);
+
+library.add(fas, far, fab);
+
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+app.component('FontAwesomeLayers', FontAwesomeLayers);
+app.component('FontAwesomeLayersText', FontAwesomeLayersText);
 
 app.mount('#app');
