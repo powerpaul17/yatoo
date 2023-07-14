55fe5cefbed22524ec09912a514b6c0e90752814
Paul Tirk <paultirk@paultirk.com>
2023-07-14 10:19:00 +0200

    complete app skeleton with navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="drawer lg:drawer-open">
+  <div class="drawer h-full overflow-x-hidden lg:drawer-open">
     <input
       id="navigation-drawer"
       type="checkbox"
       class="drawer-toggle"
     >
-    <div class="drawer-content flex flex-col overflow-x-hidden p-3" />
+    <div class="drawer-content flex flex-col">
+      <Suspense>
+        <router-view />
+      </Suspense>
+    </div>
+
+    <NavigationBar />
   </div>
 </template>
 
 <script setup lang="ts">
+
+  import NavigationBar from './components/NavigationBar.vue';
 
 </script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="drawer-side">
+    <label
+      for="navigation-drawer"
+      class="drawer-overlay"
+    />
+
+    <div class="flex h-full w-80 flex-col bg-base-200">
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
