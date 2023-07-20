64c3f38b21145fb92c04ed635b197a87fda4f960
Paul Tirk <paultirk@paultirk.com>
2023-07-20 09:13:07 +0200

    show shadow of top navigation bar when view is scrolled down

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,12 @@
       class="drawer-toggle"
     >
     <div class="drawer-content flex flex-col overflow-y-scroll">
-      <TopNavigation />
+      <TopNavigation :scrolled="!!scrollTop" />
 
-      <div class="flex h-full overflow-x-hidden">
+      <div
+        class="flex h-full overflow-x-hidden"
+        v-scroll="onScroll"
+      >
         <div class="grow px-3">
           <Suspense>
             <router-view v-slot="{ Component, route }">
@@ -33,7 +36,17 @@
 
 <script setup lang="ts">
 
+  import { ref } from 'vue';
+  import type { useScroll } from '@vueuse/core';
+  import { vScroll } from '@vueuse/components';
+
   import NavigationBar from './components/NavigationBar.vue';
   import TopNavigation from './components/TopNavigation.vue';
+
+  const scrollTop = ref(0);
+
+  function onScroll({ y }: ReturnType<typeof useScroll>): void {
+    scrollTop.value = y.value;
+  }
 
 </script>
