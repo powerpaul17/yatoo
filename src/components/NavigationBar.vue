8881a94b96891ff9518c9301a65d5366175af595
Paul Tirk <paultirk@paultirk.com>
2023-07-20 09:18:50 +0200

    add navigation item for todo view

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -5,11 +5,21 @@
       class="drawer-overlay"
     />
 
-    <div class="flex h-full w-80 flex-col bg-base-200">
+    <div class="flex h-full w-80 flex-col overflow-y-scroll bg-base-200">
+      <ul class="menu menu-lg my-1 grow p-4 text-base-content">
+        <li>
+          <router-link :to="{ name: 'todos' }">
+            <CheckCheck />
+            {{ $t('components.NavigationBar.todos') }}
+          </router-link>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+
+  import { CheckCheck } from 'lucide-vue-next';
 
 </script>
