7d369a6e6a4aeb15a57ae202b79ade07d5c51cb1
Paul Tirk <paultirk@paultirk.com>
2023-07-27 23:34:22 +0200

    add todo list & todo item components

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="
+      rounded-md
+      bg-neutral text-neutral-content hover:bg-neutral-focus
+      focus:bg-neutral-focus
+    "
+  >
+    <div class="flex items-center p-1">
+      <div>
+      </div>
+      <div class="flex-auto">
+        {{ todo.title }}
+      </div>
+      <div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+  import { type PropType } from 'vue';
+
+  import type { Todo } from '../stores/todoStore';
+
+  defineProps({
+    todo: {
+      type: Object as PropType<Todo>,
+      required: true
+    }
+  });
+
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <ListTransition class="flex flex-col items-center">
+    <TodoItem
+      v-for="todo of todos"
+      :key="todo.id"
+      :todo="todo"
+      class="mb-1 w-full max-w-5xl"
+    />
+  </ListTransition>
+</template>
+
+<script setup lang="ts">
+
+  import type { PropType } from 'vue';
+
+  import type { Todo } from '../stores/todoStore';
+
+  import ListTransition from '../components/ListTransition.vue';
+  import TodoItem from '../components/TodoItem.vue';
+
+  defineProps({
+    todos: {
+      type: Array as PropType<Array<Todo>>,
+      required: true
+    }
+  });
+
+</script>
