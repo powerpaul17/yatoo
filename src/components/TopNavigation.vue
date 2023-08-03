20814a9415dd49894a961993e2885b8e12ca67ea
Paul Tirk <paultirk@paultirk.com>
2023-08-03 15:56:17 +0200

    add fallback value for route title translation key

--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -13,7 +13,7 @@
     </div>
 
     <div class="mx-2 min-w-0 flex-1 px-2">
-      <span class="overflow-hidden text-ellipsis">{{ $t(route.meta.titleTk) }}</span>
+      <span class="overflow-hidden text-ellipsis">{{ $t(route.meta.titleTk ?? '') }}</span>
     </div>
 
     <div class="flex-none">
