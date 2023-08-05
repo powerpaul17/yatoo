8735e10ecafccb737e311ab92f43cef0bc9ddb99
Paul Tirk <paultirk@paultirk.com>
2023-08-05 23:39:52 +0200

    add create input overlay component

---
+++ src/components/CreateInputOverlay.vue
@@ -0,0 +1,64 @@
+<template>
+  <ModalComponent
+    class="modal-top"
+    :open="open"
+  >
+    <form
+      method="dialog"
+      class="modal-box mx-auto sm:w-10/12"
+    >
+      <div class="join w-full">
+        <input
+          type="text"
+          class="input-bordered input-primary input join-item w-full text-neutral-content"
+          v-model="creationString"
+        >
+        <button
+          class="btn-primary join-item btn"
+          :disabled="!isValidInput"
+          @click="handleCreateButtonClicked"
+        >
+          <Plus />
+        </button>
+      </div>
+    </form>
+  </ModalComponent>
+</template>
+
+<script setup lang="ts">
+
+  import { computed, ref } from 'vue';
+
+  import { useTodoStore } from '../stores/todoStore';
+
+  import { Plus } from 'lucide-vue-next';
+
+  import ModalComponent from './dialogs/ModalComponent.vue';
+
+  const todoStore = useTodoStore();
+
+  defineProps({
+    open: {
+      type: Boolean,
+      required: true
+    }
+  });
+
+  const creationString = ref('');
+
+  const isValidInput = computed(() => {
+    return creationString.value.length;
+  });
+
+  async function handleCreateButtonClicked(): Promise<void> {
+    await todoStore.create({
+      title: creationString.value,
+      description: '',
+      done: false,
+      doneAt: null
+    });
+
+    creationString.value = '';
+  }
+
+</script>
