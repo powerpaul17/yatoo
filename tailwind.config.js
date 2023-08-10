8b917f0a83cb0af0f58f1836f007b30950c29d3d
Paul Tirk <paultirk@paultirk.com>
2023-08-10 22:14:44 +0200

    set some better theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,4 +12,20 @@ export default {
   plugins: [
     require('daisyui')
   ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: colors.sky[400],
+          neutral: colors.neutral[300],
+          'base-100': colors.neutral[100],
+        },
+        dark: {
+          primary: colors.sky[400],
+          neutral: colors.neutral[500],
+          'base-100': colors.neutral[700]
+        }
+      }
+    ]
+  }
 }
