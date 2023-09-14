3b8bea79b2061f4e623ddf3e28e922c1c01e1410
Paul Tirk <paultirk@paultirk.com>
2023-09-14 21:12:49 +0200

    ensure a local storage is always returned

--- src/stores/LocalStorage/useLocalStorage.ts
+++ src/stores/LocalStorage/useLocalStorage.ts
@@ -1,19 +1,22 @@
 import process from 'process';
 
-import type {LocalStorage} from './LocalStorage';
+import type { LocalStorage } from './LocalStorage';
 import { MemoryLocalStorage } from './MemoryLocalStorage';
 
 const localStorageMap: Map<string, LocalStorage<any>> = new Map();
 
 export const useLocalStorage = async <T>(tableName: string): Promise<LocalStorage<T>> => {
-  if (!localStorageMap.has(tableName)) {
+  let localStorage = localStorageMap.get(tableName);
+
+  if (!localStorage) {
     if (process.env.BUN_TEST) {
-      localStorageMap.set(tableName, new MemoryLocalStorage(tableName));
+      localStorage = new MemoryLocalStorage(tableName);
     } else {
       const localForageLocalStorage = await import('./LocalForageLocalStorage');
-      localStorageMap.set(tableName, new localForageLocalStorage.LocalForageLocalStorage(tableName));
+      localStorage = new localForageLocalStorage.LocalForageLocalStorage(tableName);
     }
+    localStorageMap.set(tableName, localStorage);
   }
 
-  return localStorageMap.get(tableName);
+  return localStorage;
 };
