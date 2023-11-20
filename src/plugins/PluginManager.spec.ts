95a884dab6677bc903cf58831f6b9061838b6ec5
Paul Tirk <paultirk@paultirk.com>
2023-11-20 13:18:19 +0100

    add test for initializing plugins from plugin manager

---
+++ src/plugins/PluginManager.spec.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from 'bun:test';
+import sinon from 'sinon';
+
+import { PluginManager } from './PluginManager';
+import { Plugin } from './Plugin';
+
+describe('PluginManager', () => {
+  it('should initialize a registered plugin', async () => {
+    const testPlugin = new TestPlugin();
+    const initSpy = sinon.spy(testPlugin, 'init');
+
+    const pluginManager = new PluginManager(testPlugin);
+    await pluginManager.init();
+
+    expect(initSpy.callCount).toBe(1);
+  });
+});
+
+class TestPlugin extends Plugin {
+  public getPluginId(): string {
+    return 'test-plugin';
+  }
+
+  public init(): Promise<void> {
+    return Promise.resolve();
+  }
+}
