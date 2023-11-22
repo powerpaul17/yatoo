041c883d8cd6523cabcb0df3b2b0ff6f07e412b0
Paul Tirk <paultirk@paultirk.com>
2023-11-22 21:45:00 +0100

    add utility function to calculate differences between two arrays

--- src/classes/Utils.ts
+++ src/classes/Utils.ts
@@ -31,6 +31,39 @@ export class Utils {
     return f;
   }
 
+  public static computeArrayElementDifferences<T>(
+    array1: Array<T>,
+    array2: Array<T>,
+    compareFunction: (item1: T, item2: T) => boolean
+  ): {
+    onlyInArray1: Array<T>;
+    onlyInArray2: Array<T>;
+  } {
+    const array1Copy = array1.slice();
+    const array2Copy = array2.slice();
+
+    const onlyInArray1: Array<T> = [];
+
+    while (array1Copy.length) {
+      const item1 = array1Copy.shift();
+      if (item1 == null) continue;
+
+      const foundItem2Index = array2Copy.findIndex((item2) => {
+        return compareFunction(item1, item2);
+      });
+
+      if (foundItem2Index >= 0) {
+        array2Copy.splice(foundItem2Index, 1);
+      } else {
+        onlyInArray1.push(item1);
+      }
+    }
+
+    return {
+      onlyInArray1,
+      onlyInArray2: array2Copy
+    };
+  }
 }
 
 type RateLimitedFunction<F extends (...args: Array<any>) => void | Promise<void>> = {
