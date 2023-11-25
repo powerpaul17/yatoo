a3344dce24bcc6227a5d4d72dfe4643e3e566071
Paul Tirk <paultirk@paultirk.com>
2023-11-25 22:50:24 +0100

    format TopNavigation.vue

--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -12,7 +12,9 @@
     </div>
 
     <div class="mx-2 min-w-0 flex-1 px-2">
-      <span class="overflow-hidden text-ellipsis">{{ $t(route.meta.titleTk ?? '') }}</span>
+      <span class="overflow-hidden text-ellipsis">
+        {{ $t(route.meta.titleTk ?? '') }}
+      </span>
     </div>
 
     <div class="flex-none">
@@ -31,7 +33,6 @@
 </template>
 
 <script setup lang="ts">
-
   import { ref } from 'vue';
   import { useRoute } from 'vue-router';
 
@@ -54,5 +55,4 @@
   }>();
 
   const createInputOverlayOpen = ref(false);
-
 </script>
