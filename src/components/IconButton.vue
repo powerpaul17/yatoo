2529eab14d7dc5437efd215ca35cbd2d0e96a15d
Paul Tirk <paultirk@paultirk.com>
2023-11-25 22:45:49 +0100

    add icon button component

---
+++ src/components/IconButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <Button
+    text
+    icon
+    plain
+    rounded
+    @click="emit('click')"
+  >
+    <template #icon="{ class: cls }">
+      <slot
+        name="icon"
+        :class="cls"
+      ></slot>
+    </template>
+  </Button>
+</template>
+
+<script setup lang="ts">
+  import Button from 'primevue/button';
+
+  const emit = defineEmits<{
+    (event: 'click'): void;
+  }>();
+</script>
