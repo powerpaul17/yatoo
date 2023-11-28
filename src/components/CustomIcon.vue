1afe32dc7cee9bfba222577e06b3fbec30701441
Paul Tirk <paultirk@paultirk.com>
2023-11-28 21:43:25 +0100

    add custom icon component

---
+++ src/components/CustomIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="icon" />
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue';
+
+  import * as icons from 'lucide-vue-next';
+
+  const props = defineProps({
+    name: {
+      type: String,
+      required: true
+    }
+  });
+
+  const icon = computed(() => {
+    return icons[props.name];
+  });
+</script>
