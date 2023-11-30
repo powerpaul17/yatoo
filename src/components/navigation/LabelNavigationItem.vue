a069d6df9f7ddf6f19934fc1949acad271b5ecbb
Paul Tirk <paultirk@paultirk.com>
2023-11-30 16:31:17 +0100

    add label navigation item

---
+++ src/components/navigation/LabelNavigationItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <NavigationItem
+    :title="
+      label.name ||
+      `(${$t('components.navigation.LabelNavigationItem.noLabelName')})`
+    "
+    :route="{
+      name: 'label',
+      params: {
+        labelId: label.id
+      }
+    }"
+  >
+    <template #icon>
+      <CustomIcon
+        v-if="label.icon"
+        :name="label.icon"
+      />
+      <Tag v-else />
+    </template>
+  </NavigationItem>
+</template>
+
+<script setup lang="ts">
+  import { ref, type PropType } from 'vue';
+
+  import type { Label } from '../../stores/labelStore';
+
+  import { Tag } from 'lucide-vue-next';
+
+  import NavigationItem from './NavigationItem.vue';
+  import CustomIcon from '../CustomIcon.vue';
+
+  const props = defineProps({
+    label: {
+      type: Object as PropType<Label>,
+      required: true
+    }
+  });
+</script>
