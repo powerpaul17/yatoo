5d69ea18b2f9273d83fc4fb38e9723a26aeae197
Paul Tirk <paultirk@paultirk.com>
2023-11-30 12:01:28 +0100

    add label item component

---
+++ src/components/labels/LabelItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <span
+    class="inline-flex h-5 items-center justify-center rounded-full border pl-0.5 pr-2"
+    :class="{
+      '!bg-transparent': !label.color
+    }"
+    :style="{
+      'background-color': label.color,
+      'border-color': label.color || 'black',
+    }"
+  >
+    <CustomIcon
+      v-if="label.icon"
+      :name="label.icon"
+      class="h-full w-full py-0.5"
+    />
+    <Tag
+      v-else
+      class="h-full w-full py-0.5"
+    />
+    <span class="ml-1">
+      {{ label.name }}
+    </span>
+  </span>
+</template>
+
+<script setup lang="ts">
+  import type { PropType } from 'vue';
+
+  import { Tag } from 'lucide-vue-next';
+
+  import CustomIcon from '../CustomIcon.vue';
+
+  import type { Label } from '../../stores/labelStore';
+
+  defineProps({
+    label: {
+      type: Object as PropType<Label>,
+      required: true
+    }
+  });
+</script>
