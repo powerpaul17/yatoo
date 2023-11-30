495da21754e39a70a34fe505f4422cebd9a71ff8
Paul Tirk <paultirk@paultirk.com>
2023-11-30 12:03:25 +0100

    add label list component

---
+++ src/components/labels/LabelList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="-mb-1">
+    <LabelItem
+      v-for="label of labels"
+      class="mb-1 mr-1 last:mr-0"
+      :key="label.id"
+      :label="label"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import type { PropType } from 'vue';
+
+  import LabelItem from './LabelItem.vue';
+
+  import type { Label } from '../../stores/labelStore';
+
+  defineProps({
+    labels: {
+      type: Array as PropType<Array<Label>>,
+      required: true
+    }
+  });
+</script>
