3759b116aa1cab615579d4efab1a3720b5e22b18
Paul Tirk <paultirk@paultirk.com>
2024-03-29 21:14:11 +0100

    rename BUN_TEST env variable to TEST

--- src/stores/LocalStorage/useLocalStorage.ts
+++ src/stores/LocalStorage/useLocalStorage.ts
@@ -9,7 +9,7 @@ export const useLocalStorage = async <T>(
   let localStorage = localStorageMap.get(tableName);
 
   if (!localStorage) {
-    if (process.env.BUN_TEST) {
+    if (process.env.TEST) {
       localStorage = new MemoryLocalStorage(tableName);
     } else {
       const localForageLocalStorage = await import('./LocalForageLocalStorage');
