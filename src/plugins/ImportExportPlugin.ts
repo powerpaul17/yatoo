bd858f33a5e12f9bd9b44cb859fadb64c530d511
Paul Tirk <paultirk@paultirk.com>
2024-04-05 16:35:48 +0200

    remove message bus class

--- src/plugins/ImportExportPlugin.ts
+++ src/plugins/ImportExportPlugin.ts
@@ -1,5 +1,3 @@
-import { useMessageBus, type MessageConfig } from '../classes/MessageBus';
-import type { Entity } from '../stores/Store';
 import { Plugin } from './Plugin';
 
 export class ImportExportPlugin extends Plugin {
@@ -8,17 +6,6 @@ export class ImportExportPlugin extends Plugin {
   }
 
   public async init(): Promise<void> {
-    const messageBus = useMessageBus();
-
-    const { notify: notifyExport } = messageBus.registerMessage<
-      ExportMessage<any>
-    >('import-export::export');
     return Promise.resolve();
   }
 }
-
-export type ExportMessage<TEntity extends Entity> = MessageConfig<
-  'import-export::export',
-  void,
-  { entityName: string; entities: Array<TEntity> }
->;
