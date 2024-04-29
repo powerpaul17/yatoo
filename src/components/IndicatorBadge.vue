87228cfd74a9dc33918b74635e40e6d750627b4e
Paul Tirk <paultirk@paultirk.com>
2024-04-29 22:58:56 +0200

    add indicator badge component

---
+++ src/components/IndicatorBadge.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="relative inline">
+    <slot></slot>
+
+    <div
+      v-if="show"
+      class="absolute left-3/4 top-1/4 -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary-500 p-1 text-xs font-bold text-white"
+    >
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    show: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  });
+</script>
