86eb0f0ff14d2f5eb2ba755c6d6b66241a263c51
Paul Tirk <paultirk@paultirk.com>
2024-08-20 22:17:21 +0200

    split vendor & runtime into separate bundle files

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true
   },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    },
+    moduleIds: 'deterministic'
+  },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
