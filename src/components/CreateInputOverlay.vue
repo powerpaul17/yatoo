9e5bbe36a625b70a1d6338074121a2c07e200ef8
Paul Tirk <paultirk@paultirk.com>
2024-09-19 12:34:38 +0200

    use creation string parser result to create todos

--- src/components/CreateInputOverlay.vue
+++ src/components/CreateInputOverlay.vue
@@ -27,6 +27,28 @@
             </template>
           </Button>
         </InputGroup>
+
+        <div class="mt-2 flex items-baseline">
+          <span
+            v-if="!parseResult"
+            class="text-sm text-gray-400"
+          >
+            Example: "A new little task #some-tag"
+          </span>
+
+          <LabelItem
+            v-for="(label, index) of labels"
+            :key="index"
+            :label-id="label.id"
+          />
+          <span
+            v-for="(label, index) of parseResult?.newLabels"
+            class="inline-flex h-5 items-center justify-center rounded-full border border-black bg-transparent pl-0.5 pr-2"
+            :key="index"
+          >
+            {{ label }}
+          </span>
+        </div>
       </div>
     </template>
   </Sidebar>
@@ -42,14 +64,25 @@
   import Button from 'primevue/button';
 
   import { useTodoStore } from '../stores/todoStore';
+  import { useLabelStore, type Label } from '../stores/labelStore';
   import { useLabelToTodoStore } from '../stores/labelToTodoStore';
 
+  import {
+    useCreationStringParser,
+    type TodoCreationData
+  } from '../classes/CreationStringParser';
+
   import { Plus } from 'lucide-vue-next';
+
+  import LabelItem from './labels/LabelItem.vue';
 
   const route = useRoute();
 
   const todoStore = useTodoStore();
+  const labelStore = useLabelStore();
   const labelToTodoStore = useLabelToTodoStore();
+
+  const creationStringParser = useCreationStringParser();
 
   const props = defineProps({
     open: {
@@ -77,6 +110,32 @@
 
   const creationString = ref('');
 
+  const parseResult = ref<TodoCreationData | null>(null);
+
+  watch(creationString, async () => {
+    const result = await creationStringParser.parseCreationString(
+      creationString.value
+    );
+    parseResult.value = result;
+  });
+
+  const labels = ref<Array<Label>>([]);
+
+  watch(parseResult, async () => {
+    if (!parseResult.value) {
+      labels.value = [];
+    } else {
+      const newLabels = [];
+      for (const labelId of parseResult.value.labelIds) {
+        const label = await labelStore.getById(labelId);
+        if (!label) throw new Error('label does not exist');
+
+        newLabels.push(label);
+      }
+      labels.value = newLabels;
+    }
+  });
+
   const isValidInput = computed(() => {
     return creationString.value.length;
   });
@@ -90,12 +149,31 @@
   }
 
   async function createTodo(): Promise<void> {
+    if (!parseResult.value) return;
+
+    const labelIds = parseResult.value.labelIds.slice();
+    for (const newLabel of parseResult.value.newLabels) {
+      const id = await labelStore.create({
+        name: newLabel,
+        color: '',
+        icon: ''
+      });
+      labelIds.push(id);
+    }
+
     const todoId = await todoStore.create({
-      title: creationString.value,
+      title: parseResult.value.title,
       description: '',
       done: false,
       doneAt: null
     });
+
+    for (const labelId of labelIds) {
+      await labelToTodoStore.create({
+        labelId,
+        todoId
+      });
+    }
 
     const labelId = route.params['labelId'];
     if (typeof labelId === 'string') {
