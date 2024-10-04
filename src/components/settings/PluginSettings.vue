f60c57892f4bb6d214e23fe80e8ccd588bb131b3
Paul Tirk <paultirk@paultirk.com>
2024-10-04 17:10:30 +0200

    show plugin settings in settings view

---
+++ src/components/settings/PluginSettings.vue
@@ -0,0 +1,23 @@
+<template>
+  <template v-for="section of pluginSettings">
+    <SettingsSection
+      v-if="section"
+      :key="section.name"
+      :section="section"
+      class="my-2"
+    />
+  </template>
+</template>
+
+<script setup lang="ts">
+  import { inject, onUnmounted } from 'vue';
+
+  import SettingsSection from './SettingsSection.vue';
+  import { PluginManagerKey } from '../../provideKeys';
+
+  const pluginManager = inject(PluginManagerKey);
+
+  const pluginSettings = pluginManager?.getSettings();
+
+  onUnmounted(() => {});
+</script>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div></div>
+  <div>
+    <div
+      id="plugin-settings"
+      class="my-8 text-center text-lg"
+    >
+      {{ $t('views.settings.plugins') }}
+    </div>
+
+    <PluginSettings />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import PluginSettings from '../components/settings/PluginSettings.vue';
+</script>
