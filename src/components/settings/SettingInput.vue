0a4f447258021622cba27ed36b77b013a477df84
Paul Tirk <paultirk@paultirk.com>
2024-10-04 17:10:30 +0200

    add setting input component

---
+++ src/components/settings/SettingInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <component :is="getComponentForType(settingDefinition)"></component>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { type PropType, type Component } from 'vue';
+
+  import { type SettingDefinition } from '../../types/SettingsTypes';
+
+  defineProps({
+    settingDefinition: {
+      type: Object as PropType<SettingDefinition>,
+      required: true
+    }
+  });
+
+  function getComponentForType(definition: SettingDefinition): Component {
+    switch (definition.type) {
+      default:
+        throw new Error(
+          `component for setting input type ${definition.type} not implemented`
+        );
+    }
+  }
+</script>
