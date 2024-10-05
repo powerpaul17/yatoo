56e1c260d6eeeb3dfa55eef476b9bfd4b2ac3475
Paul Tirk <paultirk@paultirk.com>
2024-10-05 12:07:45 +0200

    improve layout of settings sections

--- src/components/settings/SettingsSection.vue
+++ src/components/settings/SettingsSection.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="bg-surface-300 p-2 dark:bg-surface-700">
-    <div class="text-xl font-medium">
+  <div
+    class="w-full max-w-5xl rounded-md bg-surface-300 p-4 dark:bg-surface-700"
+  >
+    <div class="mb-6 text-xl font-medium">
       <h2>{{ $t(section.labelTk) }}</h2>
     </div>
     <div class="">
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center">
     <div
       id="plugin-settings"
       class="my-8 text-center text-lg"
