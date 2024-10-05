207c6058d9723872e43753d442ed7daf71a98a00
Paul Tirk <paultirk@paultirk.com>
2024-10-05 16:07:35 +0200

    add test for registering plugin settings

--- src/plugins/PluginManager.spec.ts
+++ src/plugins/PluginManager.spec.ts
@@ -1,8 +1,9 @@
 import { describe, expect, it } from 'vitest';
 import sinon from 'sinon';
 
-import { PluginManager } from './PluginManager';
+import { PluginManager, type PluginInitOptions } from './PluginManager';
 import { Plugin } from './Plugin';
+import { SettingInputType, type SettingsConfig } from '../types/SettingsTypes';
 
 describe('PluginManager', () => {
   it('should initialize a registered plugin', async () => {
@@ -17,7 +18,7 @@ describe('PluginManager', () => {
 
   describe('getPlugin', () => {
     it('should return a plugin if it is available', () => {
-      const testPlugin = new TestPlugin({});
+      const testPlugin = new TestPlugin();
       const pluginManager = new PluginManager(testPlugin);
 
       const plugin = pluginManager.getPlugin(TestPlugin);
@@ -31,14 +32,57 @@ describe('PluginManager', () => {
       expect(plugin).toBe(null);
     });
   });
+
+  describe('getSettings', () => {
+    it('should return none if there are no settings defined', () => {
+      const testPlugin = new TestPlugin();
+      const pluginManager = new PluginManager(testPlugin);
+
+      const settings = pluginManager.getSettings();
+      expect(settings).toEqual([]);
+    });
+
+    it('should return defined settings', () => {
+      const pluginSettings: Array<SettingsConfig> = [
+        {
+          name: 'setting',
+          labelTk: 'setting',
+          settings: {
+            testSetting1: {
+              name: 'test',
+              labelTk: 'test',
+              type: SettingInputType.STRING
+            }
+          }
+        }
+      ];
+      const testPlugin = new TestPlugin({ settings: pluginSettings });
+      const pluginManager = new PluginManager(testPlugin);
+
+      pluginManager.init();
+
+      const settings = pluginManager.getSettings();
+      expect(settings).toEqual(pluginSettings);
+    });
+  });
 });
 
 class TestPlugin extends Plugin {
+  private readonly settings: Array<SettingsConfig> = [];
+
+  constructor(options?: { settings?: Array<SettingsConfig> }) {
+    super();
+
+    if (options?.settings) this.settings.push(...options.settings);
+  }
+
   public getPluginId(): string {
     return 'test-plugin';
   }
 
-  public init(): Promise<void> {
+  public init({ registerSettings }: PluginInitOptions): Promise<void> {
+    registerSettings(this.settings);
+
     return Promise.resolve();
   }
 }
