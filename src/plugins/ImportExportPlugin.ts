6c410958bc271aae225bf46b9587c89833aec367
Paul Tirk <paultirk@paultirk.com>
2024-10-05 11:30:46 +0200

    add button for importing data

--- src/plugins/ImportExportPlugin.ts
+++ src/plugins/ImportExportPlugin.ts
@@ -1,7 +1,13 @@
-import { useStorageManager } from '../stores/StorageManager';
+import { useI18n } from 'vue-i18n';
+
+import {
+  ZodImportExportFormat,
+  useStorageManager
+} from '../stores/StorageManager';
 import { SettingInputType } from '../types/SettingsTypes';
 import { Plugin } from './Plugin';
 import type { PluginInitOptions } from './PluginManager';
+import { Logger } from '../classes/Logger';
 
 export class ImportExportPlugin extends Plugin {
   private storageManager = useStorageManager();
@@ -20,6 +26,41 @@ export class ImportExportPlugin extends Plugin {
             name: 'importExportButtons',
             type: SettingInputType.INPUT_GROUP,
             children: [
+              {
+                name: 'import',
+                type: SettingInputType.FILE,
+                labelTk: 'plugins.importExportPlugin.import',
+                accept: 'application/json',
+                handler: async (file): Promise<void> => {
+                  const fileContents = await new Promise<string>((res, rej) => {
+                    const reader = new FileReader();
+
+                    reader.onload = (): void => {
+                      res(reader.result as string);
+                    };
+
+                    reader.onerror = (err): void => {
+                      rej(err);
+                    };
+
+                    reader.readAsText(file);
+                  });
+
+                  try {
+                    const data = JSON.parse(fileContents) as unknown;
+
+                    const parsedData = ZodImportExportFormat.parse(data);
+
+                    await this.storageManager.importData(parsedData);
+                  } catch (error) {
+                    Logger.error(
+                      this.getPluginId(),
+                      'wrong file format',
+                      error
+                    );
+                  }
+                }
+              },
               {
                 name: 'export',
                 labelTk: 'plugins.importExportPlugin.export',
